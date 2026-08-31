c8f97315ac5b1643f921f976fee054d7feedb234
dushyantsharma3244 <dushyantsharma3244@gmail.com>
2026-08-31 16:17:03 +0530

    Initial commit

---
+++ MY_PROJECT.py
@@ -0,0 +1,43 @@
+import joblib
+import streamlit as st 
+import nltk
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+from nltk.stem import PorterStemmer
+nltk.download("stopwords")
+nltk.download("punkt")
+nltk.download("punkt_tab")
+import re
+import string
+
+# model =joblib.load(r'E:\PYTHON\spam_mnb_model.pkl')
+# vector=joblib.load('E:\PYTHON\count_vectorizer.pkl')
+model = joblib.load("spam_mnb_model.pkl")
+vector = joblib.load("count_vectorizer.pkl")
+st.header('EMAIL/SMS SPAM CLASSIFIER')
+porter=PorterStemmer()
+stop_word=set(stopwords.words('english'))
+user_input=st.text_area('enter you email')
+def tranform_text(text):
+  text=re.sub(r'[%#@]','',text)
+  text = re.sub(r'_+', ' ', text)
+  text=text.lower()
+  text=word_tokenize(text)
+  new_text=[]
+  for i in text:
+    if i not in stop_word and i not in string.punctuation:
+      i=porter.stem(i)
+      new_text.append(i)
+
+
+  return  ' '.join(new_text)
+
+if st.button('predict'):
+ tranform_input=tranform_text(user_input)
+ dense_form=vector.transform([tranform_input])
+ prediction=model.predict(dense_form)
+
+ if prediction[0]==1:
+   st.write('🚨ITS A SPAM')
+ else:
+   st.write('✅ NOT SPAM') 
